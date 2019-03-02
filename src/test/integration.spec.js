93b6a4e5fc1c8a2f11e7292f915e5298c703be81
dragfire <asem.devajit@gmail.com>
2019-03-02 12:56:41 -0500

    Add appointments tests

--- src/test/integration.spec.js
+++ src/test/integration.spec.js
@@ -9,6 +9,8 @@ chai.use(chaiAsPromised);
 chai.use(chaiHttp);
 chai.should();
 
+const DATETIME_FORMAT = "YYYY-MM-DD HH:mm:ss";
+
 const patient = {
     firstName: "Devajit",
     lastName: "Asem",
@@ -25,7 +27,7 @@ const doctor = {
 
 let patients = [];
 
-describe("REST API Integration test", () => {
+describe("API Integration test", () => {
     describe("Patients", () => {
         describe("/POST patient", () => {
             it("should create a Patient", (done) => {
@@ -54,6 +56,7 @@ describe("REST API Integration test", () => {
         //     done();
         // })
     });
+
     describe("Doctors", () => {
         describe("/POST doctor", () => {
             it("should create a Doctor", (done) => {
@@ -82,15 +85,35 @@ describe("REST API Integration test", () => {
 
         describe("/POST /doctor/:id/schedule", () => {
             const schedules = [];
-
+            const now = moment(new Date());
             schedules.push({
-                availableFrom: moment(new Date()).format("")
-            })
+                availableFrom: now.format(DATETIME_FORMAT),
+                availableTo: now.add(3, 'h').format(DATETIME_FORMAT)
+            });
 
             it("should create a Doctor's list of schedules", (done) => {
                 chai.request(app)
-                    .put("/api/doctor/1")
-                    .send(updateData)
+                    .post("/api/doctor/1/schedule")
+                    .send(schedules)
+                    .then((res) => {
+                        res.should.have.status(201);
+                        done();
+                    });
+            });
+        });
+
+        describe("/PUT /doctor/:id/schedule", () => {
+            const schedules = [];
+            const now = moment(new Date());
+            schedules.push({
+                availableFrom: now.add(3, 'h').format(DATETIME_FORMAT),
+                availableTo: now.add(6, 'h').format(DATETIME_FORMAT)
+            });
+
+            it("should update a Doctor's list of schedules", (done) => {
+                chai.request(app)
+                    .put("/api/doctor/1/schedule")
+                    .send(schedules)
                     .then((res) => {
                         res.should.have.status(204);
                         done();
@@ -98,19 +121,35 @@ describe("REST API Integration test", () => {
             });
         });
 
-        // describe("/GET patient", () => {
-        //     it("should get all Patients", (done) => {
-        //         chai.request(app)
-        //             .get("/api/patient")
-        //             .then((res) => {
-        //                 res.should.have.status(200);
-        //                 done();
-        //             });
-        //     });
-        // });
+        describe("/GET /doctor/:id/schedule", () => {
+            it("should get a Doctor's list of schedules", (done) => {
+                chai.request(app)
+                    .get("/api/doctor/1/schedule")
+                    .then((res) => {
+                        res.should.have.status(200);
+                        done();
+                    });
+            });
+        });
+    });
 
-        // it("should get Patient's appointments", (done) => {
-        //     done();
-        // })
+    describe("Appointments", () => {
+        describe("/POST /appointment/patient/:patientId/doctor/:doctorId", () => {
+            const now = moment();
+            const appointment = {
+                startAt: now.add(8, 'h').format(DATETIME_FORMAT),
+                endAt: now.add(9, 'h').format(DATETIME_FORMAT)
+            };
+
+            it("should create an appointment", (done) => {
+                chai.request(app)
+                    .post("/api/appointment/patient/1/doctor/1")
+                    .send(appointment)
+                    .then((res) => {
+                        res.should.have.status(201);
+                        done();
+                    });
+            });
+        });
     });
 });
