b697a029201d6374297d75f5f5d2fb2b82a2f9ab
dragfire <asem.devajit@gmail.com>
2019-03-02 01:03:41 -0500

    Return appointment data when transaction is committed.

--- src/services/appointment-service.js
+++ src/services/appointment-service.js
@@ -17,28 +17,40 @@ const bookAppointment = async (doctorId, patientId, bookingDetails) => {
     if (!doctor) {
         throw boom.notFound("Doctor not found with id: " + doctorId);
     }
+
+    // Check if patient has any appointment overlapped with this booking details
     const isPatientAlreadyBooked = await patientService.isPatientAlreadyBooked(patient, bookingDetails);
     if (isPatientAlreadyBooked) {
-        throw boom.conflict("Patient is already booked during this period");
+        throw boom.conflict("Patient is booked during this period");
     }
 
+    // Check if doctor has any appointment overlapped with this booking details
     const isDoctorAlreadyBooked = await doctorService.isDoctorAlreadyBooked(doctor, bookingDetails);
     if (isDoctorAlreadyBooked) {
-        throw boom.conflict("Doctor is already booked during this period");
+        throw boom.conflict("Doctor is booked during this period");
     }
 
+    // Check if this booking details is within Doctor's available schedule
+    const isDoctorAvailable = await doctorService.isDoctorAvailable(doctor, bookingDetails);
+    if (isDoctorAvailable) {
+        throw boom.notAcceptable("Doctor not available during this period");
+    }
+
+    // Start a transaction for booking the appointment
     return sequelize.transaction().then((t) => {
         return Appointment.create(bookingDetails, {transaction: t})
             .then(appointment => {
                 return doctor.addAppointment(appointment, {transaction: t})
                     .then(() => {
                         return patient.addAppointment(appointment, {transaction: t}).then(() => {
+                            // Everything went fine commit
                             t.commit();
+                            return appointment;
                         });
                     });
             });
-    }).then(() => {
-        console.info("Booked appointment successfully");
+    }).then((appointment) => {
+        console.info("bookAppointment: Booked appointment successfully with id:", appointment.id);
     });
 };
 
