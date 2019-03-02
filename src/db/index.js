19fff692a7f4dbd2fdadfd1405dda6fc24521e67
dragfire <asem.devajit@gmail.com>
2019-03-02 01:02:03 -0500

    Handle test environment

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,25 @@
 import Sequelize from 'sequelize';
 import config from '../config';
 
-const sequelize = new Sequelize('luma', 'root', 'devmysql', { 
-  host: config.get("db").host,
-  dialect: 'mysql'
+const dbConfig = config.get("db");
+// console.info(dbConfig)
+
+const isTestEnv = () => process.env.NODE_ENV === 'test';
+
+const sequelize = new Sequelize(dbConfig.name.default, 'root', 'devmysql', {
+  host: dbConfig.host.default,
+  dialect: "mysql",
+  logging: !isTestEnv()
 });
 
-sequelize.sync();
+const syncOptions = {
+  force: false
+};
+
+if (isTestEnv()) {
+  syncOptions.force = true;
+}
+
+sequelize.sync(syncOptions);
 
 export default sequelize;
