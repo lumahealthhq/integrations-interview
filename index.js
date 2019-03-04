4fd6b91357c241ceaf97f6c7e14b3f1ddbc13521
Pengfei He <zjuhpf@outlook.com>
2019-03-03 21:22:05 -0800

    server

--- index.js
+++ index.js
@@ -24,3 +24,65 @@ app.get("/appointments/:name", (req, res) => {
   if (!doctor) res.status(404).send("Doctor not found");
   res.send(doctor.appointments);
 });
+
+// create a doctor working hours
+app.post("/workingHours", (req, res) => {
+  //validate the import doctor data
+  if (!checker.checkValidDoctor(req.body)) {
+    res.status(404).send("Invalid data");
+    return;
+  }
+  // update or create a doctor working hours
+  const input = {
+    name: req.body.name,
+    workingHours: req.body.workingHours,
+    appointments: []
+  };
+  // if there is not such doctor, add this doctors
+  // if there is already this doctor, update his or her workingHours
+  const doctor = doctors.find(d => d.name === input.name);
+  if (!doctor) {
+    doctors.push(input);
+    res.send(input).send("Woking hour added");
+  } else {
+    doctor.workingHours = input.workingHours;
+    res.send(input).send("Working hour updated");
+  }
+});
+
+// book a doctor opening
+app.post("/appointments", (req, res) => {
+  if (!checker.checkValidAppointments(req.body)) {
+    res.status(404).send("Invalid input");
+  }
+  //create a new appointment
+  const input = {
+    patientName: req.body.patientName,
+    doctorName: req.body.doctorName,
+    appointDate: req.body.appointDate,
+    appointTime: req.body.appointTime
+  };
+  //check whether overlap with an existing appointment
+  const doctor = doctors.find(d => d.name === req.body.doctorName);
+  if (!doctor) {
+    res.status(404).send("Doctor not found");
+    return;
+  }
+  //check whether overlap with an existing appointment
+  const appointments = doctor.appointments;
+  if (!checker.isWork(input, doctor.workingHours)) {
+    res.send("Doctor not working at this time");
+    return;
+  }
+  if (!checker.isOverLap(input, appointments)) {
+    appointments.push(input);
+    res.send(input);
+  } else {
+    res.send("Not an opening");
+  }
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("Listening on port ${port}"));
+
+module.exports = app;
