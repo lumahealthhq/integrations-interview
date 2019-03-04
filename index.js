dcc16eabf663ebf07b14844d24930d4bfba602ba
xingches <xingches@usc.edu>
2019-03-04 12:49:59 -0800

    Generalized config.json and rename app.js to index.js

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
 
-/**
- * Adds commas to a number
- * @param {number} number
- * @param {string} locale
- * @return {string}
- */
-module.exports = function(number, locale) {
-    return number.toLocaleString(locale);
-};
+const config = require('./config.json');
+const workingHours = require('./routes/workinghour');
+const appointments = require('./routes/appointment');
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/workingHours', workingHours);
+app.use('/appointments', appointments);
+
+app.listen(config.PORT, "0.0.0.0");
+console.log('we live on ' + config.PORT);
+
+module.exports = app;
