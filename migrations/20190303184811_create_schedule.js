ca21fd46716973aa3a37201cfb27e8861257ecc4
Harsh Agrawal <hrsh.agrwl1@gmail.com>
2019-03-05 12:17:34 -0500

    Add foreign keys

--- migrations/20190303184811_create_schedule.js
+++ migrations/20190303184811_create_schedule.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('schedule', function(t) {
     t.increments('id').unsigned().primary();
-    t.integer('doctor_id').notNull();
+    t.integer('doctor_id').unsigned().index().references('id').inTable('doctors').onDelete('CASCADE');
     t.datetime('from').notNull();
     t.datetime('to').notNull();
     t.integer('status').notNull();
--- migrations/20190303185029_create_appointments.js
+++ migrations/20190303185029_create_appointments.js
@@ -2,8 +2,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('appointments', function(t) {
     t.increments('id').unsigned().primary();
-    t.integer('schedule_id').notNull();
-    t.integer('patient_id').notNull();
+    t.integer('schedule_id').unsigned().index().references('id').inTable('schedule')
+    t.integer('patient_id').unsigned().index().references('id').inTable('patients')
     t.unique(['schedule_id', 'patient_id'])
   });
 };
