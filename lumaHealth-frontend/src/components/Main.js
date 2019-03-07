e2be81b31f87656d09e6a24a2f1a9ff29dfb5fec
saumya2904 <saumya.iis@gmail.com>
2019-03-07 12:55:32 -0800

    Changes to Backend

--- lumaHealth-frontend/src/components/Main.js
+++ lumaHealth-frontend/src/components/Main.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import {Route} from 'react-router-dom';
 import Login from './Login/Login';
 import Home from './Home/Home';
-import Delete from './Delete/Delete';
 import Create from './Create/Create';
 import Add_hours from './Add_hours/Add_hours';
 import Book from './Book/Book'
@@ -16,7 +15,6 @@ class Main extends Component {
                 <Route path="/" component={Navbar}/>
                 <Route path="/login" component={Login}/>
                 <Route path="/home" component={Home}/>
-                <Route path="/delete" component={Delete}/>
                 <Route path="/create" component={Create}/>
                 <Route path="/add_hours" component={Add_hours}/>
                 <Route path="/book" component={Book}/>
